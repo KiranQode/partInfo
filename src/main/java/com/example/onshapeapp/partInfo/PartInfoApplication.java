1269317bfb7db81f1f34289f7d964825d8887d3d
KiranQode <kirankumart@hotmail.com>
2025-12-29 13:13:22 +0530

    refactor for local run

--- src/main/java/com/example/onshapeapp/partInfo/PartInfoApplication.java
+++ src/main/java/com/example/onshapeapp/partInfo/PartInfoApplication.java
@@ -1,5 +1,6 @@
 package com.example.onshapeapp.partInfo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PartInfoApplication {
 
 	public static void main(String[] args) {
+		// 1. Load .env file from the root directory
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./") // Explicitly look in the root
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        // 2. Manually set them as System Properties so Spring @Value can see them
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+		// 3. Now start Spring
 		SpringApplication.run(PartInfoApplication.class, args);
 	}
 
