0e0173c3b25a1e34c230bb78358cd1783fef6c4a
KiranQode <kirankumart@hotmail.com>
2025-12-29 11:51:54 +0530

    Add html result rendering

--- src/main/java/com/example/onshapeapp/partInfo/controller/OnshapeController.java
+++ src/main/java/com/example/onshapeapp/partInfo/controller/OnshapeController.java
@@ -1,27 +1,52 @@
 package com.example.onshapeapp.partInfo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.onshapeapp.partInfo.model.PhysicalProperties;
 import com.example.onshapeapp.partInfo.service.OnshapeService;
 
-@RestController
-@RequestMapping("/api/onshape")
+// @RestController
+// @RequestMapping("/api/onshape")
+// public class OnshapeController {
+//     private final OnshapeService onshapeService;
+
+//     public OnshapeController(OnshapeService onshapeService){
+//         this.onshapeService = onshapeService;
+//     }
+
+//     @GetMapping("/properties/{docID}/{wID}/{eID}")
+//     public PhysicalProperties getProperties(
+//         @PathVariable String docID,
+//         @PathVariable String wID,
+//         @PathVariable String eID){
+//             return onshapeService.getPartStudioPhysicalProperties(docID, wID, eID);
+//         }
+// }
+
+@Controller // This is important for returning HTML
 public class OnshapeController {
+
     private final OnshapeService onshapeService;
 
-    public OnshapeController(OnshapeService onshapeService){
+    public OnshapeController(OnshapeService onshapeService) {
         this.onshapeService = onshapeService;
     }
 
-    @GetMapping("/properties/{docID}/{wID}/{eID}")
-    public PhysicalProperties getProperties(
-        @PathVariable String docID,
-        @PathVariable String wID,
-        @PathVariable String eID){
-            return onshapeService.getPartStudioPhysicalProperties(docID, wID, eID);
-        }
+    @GetMapping("/api/onshape/properties/{did}/{wid}/{eid}")
+    public String getProperties(@PathVariable String did, 
+                                @PathVariable String wid, 
+                                @PathVariable String eid, 
+                                Model model) {
+        
+        PhysicalProperties props = onshapeService.getPartStudioPhysicalProperties(did, wid, eid);
+        
+        // This makes the 'props' object available to the HTML file
+        model.addAttribute("properties", props);
+        
+        // This returns the name of the file: templates/part-info.html
+        return "part-info"; 
+    }
 }
